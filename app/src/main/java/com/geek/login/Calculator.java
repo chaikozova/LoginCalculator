9d80cd81a9f212389d6014cd15cd87fde7f8fc49
chaikozova <r.chaikozova@gmail.com>
2021-04-22 20:03:27 +0600

    login

--- app/src/main/java/com/geek/login/Calculator.java
+++ app/src/main/java/com/geek/login/Calculator.java
@@ -4,56 +4,75 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class Calculator extends AppCompatActivity {
+
+    private TextView result ,back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator);
-        Log.d("ololo", "onCreate()");
+        Log.w("ololo", "onCreate() - calculator");
+
+        result = findViewById(R.id.result);
+        result.setText(getIntent().getStringExtra("key"));
+
+        back = findViewById(R.id.back);
+        back(back);
 
     }
-
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("ololo", "onStart()");
+        Log.w("ololo", "onStart() - calculator");
+
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d("ololo", "onRestart()");
+        Log.w("ololo", "onResart() - calculator");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("ololo", "onResume()");
+        Log.w("ololo", "onResume() - calculator");
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("ololo", "onPause()");
-
+        Log.w("ololo", "onPause() - calculator");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d("ololo", "onStop()");
+        Log.w("ololo", "onStop() - calculator");
 
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("ololo", "onDestroy()");
+        Log.w("ololo", "onDestroy() - calculator");
 
+    }
+
+
+    public void back(View view) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/geek/login/LoginActivity.java
+++ app/src/main/java/com/geek/login/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.geek.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,18 +22,21 @@ public class LoginActivity extends AppCompatActivity {
     private Button btn;
     private String login, pswrd;
     private Drawable btnBackround, btnBackround_gray;
-
+    private TextView back;
+    private ConstraintLayout screen;
 
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Log.d("ololo", "onCreate()");
+        Log.w("ololo", "onCreate()");
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         btn = findViewById(R.id.btn);
+        back = findViewById(R.id.back);
+        screen = findViewById(R.id.screen);
         btnBackround = getResources().getDrawable(R.drawable.rounded_btn);
         btnBackround_gray = getResources().getDrawable(R.drawable.rounded_btn_gray);
 
@@ -88,12 +93,72 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (!login.isEmpty() && !pswrd.isEmpty()) {
                     Toast.makeText(LoginActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
-
+                    Intent intent = new Intent(LoginActivity.this, Calculator.class);
+                    intent.putExtra("key", login);
+                    startActivity(intent);
                 }
+            }
+        });
+
+        back(back);
+
+        screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this,
+                        "you've touched the screen", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("ololo", "onStart()");
 
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.w("ololo", "onResart()");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("ololo", "onResume()");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("ololo", "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("ololo", "onStop()");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("ololo", "onDestroy()");
+
+    }
+
+    public void back(View view) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
